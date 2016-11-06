267ce199e373c3fed176633f4aefef52e66a5590
Tutul33 <tutulcou@gmail.com>
2016-11-06 21:05:49 +0600

    Add files via upload

---
+++ CS.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+
+public partial class CS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ReportDocument crystalReport = new ReportDocument();
+        crystalReport.Load(Server.MapPath("~/CustomerReport.rpt"));
+        Customers dsCustomers = GetData("select * from customers");
+        crystalReport.SetDataSource(dsCustomers);
+        CrystalReportViewer1.ReportSource = crystalReport;
+    }
+
+    private Customers GetData(string query)
+    {
+        string conString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        SqlCommand cmd = new SqlCommand(query);
+        using (SqlConnection con = new SqlConnection(conString))
+        {
+            using (SqlDataAdapter sda = new SqlDataAdapter())
+            {
+                cmd.Connection = con;
+
+                sda.SelectCommand = cmd;
+                using (Customers dsCustomers = new Customers())
+                {
+                    sda.Fill(dsCustomers, "DataTable1");
+                    return dsCustomers;
+                }
+            }
+        }
+    }
+}
---
+++ VB.aspx.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data
+Imports System.Configuration
+Imports System.Data.SqlClient
+Imports CrystalDecisions.CrystalReports.Engine
+
+Partial Class VB
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim crystalReport As New ReportDocument()
+        crystalReport.Load(Server.MapPath("~/CustomerReport.rpt"))
+        Dim dsCustomers As Customers = GetData("select * from customers")
+        crystalReport.SetDataSource(dsCustomers)
+        CrystalReportViewer1.ReportSource = crystalReport
+    End Sub
+
+    Private Function GetData(query As String) As Customers
+        Dim conString As String = ConfigurationManager.ConnectionStrings("constr").ConnectionString
+        Dim cmd As New SqlCommand(query)
+        Using con As New SqlConnection(conString)
+            Using sda As New SqlDataAdapter()
+                cmd.Connection = con
+
+                sda.SelectCommand = cmd
+                Using dsCustomers As New Customers()
+                    sda.Fill(dsCustomers, "DataTable1")
+                    Return dsCustomers
+                End Using
+            End Using
+        End Using
+    End Function
+End Class
